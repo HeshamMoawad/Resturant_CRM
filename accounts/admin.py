fb2fa5a990dbc2ddefb9a3d64d0d9b2e88c2dc68
Hesham Moawad <heshammoawad120120@yahoo.com>
2023-11-04 19:03:16 +0200

    add models to admin bannel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import (
+    Account ,
+    Address,
+    Area ,
+    City ,
+)
+
 
 # Register your models here.
+admin.site.register(City)
+admin.site.register(Area)
+admin.site.register(Address)
+admin.site.register(Account)
--- accounts/models.py
+++ accounts/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class City(models.Model):
-
+    # creator = 
     name = models.CharField(max_length=50)
     created_at = models.DateField(verbose_name="Created At",auto_now=True)
 
@@ -16,11 +16,12 @@ class City(models.Model):
 
 
 class Area(models.Model):
+    # creator = 
     name = models.CharField(max_length=50)
     city = models.ForeignKey(City,on_delete=models.CASCADE)
 
     def __str__(self) -> str:
-        return f"{self.city}:{self.name}"
+        return f"{self.city} : {self.name}"
     
     class Meta:
         verbose_name = 'Area'
@@ -29,14 +30,24 @@ class Area(models.Model):
 
 
 class Account(models.Model):
+    # creator = 
     name = models.CharField(max_length=50 )
     phone_number =  models.CharField(max_length=11)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self) -> str:
+        return f"{self.name} : {self.phone_number}"
+    
+    class Meta:
+        verbose_name = 'Account'
+        verbose_name_plural = "Accounts"
+
+
 
 
 class Address(models.Model): 
+    # creator = 
     account = models.ForeignKey(Account,on_delete=models.CASCADE,related_name="addresses")                                                  
     city = models.ForeignKey(City,on_delete=models.CASCADE)
     area = models.ForeignKey(Area,on_delete=models.CASCADE)
@@ -47,7 +58,7 @@ class Address(models.Model):
     landmark = models.CharField(max_length=50 , blank=True)
 
     def __str__(self) -> str:
-        return f"{self.area}:{self.building}"
+        return f"{self.account} - {self.area} : {self.building}"
     
     class Meta:
         verbose_name = 'Address'
